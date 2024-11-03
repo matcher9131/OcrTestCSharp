bcdfca9d0387dadaa6f73606b6498cc1d54db800
matcher9131 <matcher9131cd@gmail.com>
2024-11-03 10:45:43 +0900

    プロジェクト ファイルを追加します。

---
+++ OcrTestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+
+static string AskFilePath()
+{
+    Console.Write("Input file: ");
+    return Console.ReadLine()!.Trim('"');
+}
+
+string filePath = args.LastOrDefault() ?? AskFilePath();
+using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+var decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+var bitmap = await decoder.GetSoftwareBitmapAsync();
+var engine = OcrEngine.TryCreateFromUserProfileLanguages();
+var result = await engine.RecognizeAsync(bitmap);
+Console.WriteLine(result.Text);
